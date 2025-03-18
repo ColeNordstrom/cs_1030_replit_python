c055063cdc3057dc7d6b81839c4493bff1e17d39
ColeNordstrom <colenordstrom97@gmail.com>
2025-03-18 15:44:46 -0600

    Create main.py

---
+++ lecture labs/main.py
@@ -0,0 +1,7 @@
+from random import randint
+"paper"=="p" "rock"=="r" "scissors"=="s" 
+"p1"=="player 1"
+"p2"=="player 2"
+# starting game
+if ("p1==p and p2==p or p1==r and p2==r or p1==s and p2==s")
+     print "draw"
